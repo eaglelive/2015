489b86c36cf156eb1d78cc2f1a8a1dbe34adcb40
eaglelive <eagle2009live@163.com>
2015-07-04 15:24:49 +0800

    cms

---
+++ xcms/src/test/java/maven/MavenTest.java
@@ -0,0 +1,12 @@
+package test.java.maven;
+
+import org.junit.Test;
+
+public class MavenTest {
+
+	@Test
+	public void sayHelloWord() {
+		System.out.println("Hello Word !");
+	}
+
+}
---
+++ xcms/src/test/java/zookeeper/ConnectZookeeper.java
@@ -0,0 +1,5 @@
+package test.java.zookeeper;
+
+public class ConnectZookeeper {
+
+}
